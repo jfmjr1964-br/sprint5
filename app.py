cc7e2f81e8a7c79c5321e7aafc4fec9d29c87180
Joao Mattos <jfmjr1964@gmail.com>
2026-05-20 18:04:52 -0300

    Atualizando app.py com o codigo limpo da moeda

--- app.py
+++ app.py
@@ -29,3 +29,25 @@ def toss_coin(n):
         mean = outcome_1_count / outcome_no
         chart.add_rows([mean])
         time.sleep(0.05)
+
+    return mean
+
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
+start_button = st.button('Executar')
+
+if start_button:
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    st.session_state['experiment_no'] += 1
+    mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])
